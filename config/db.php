a39ea993398879484f5d58e8162eb833333ebfbe
coordinador de sistemas <csistemas@JGEFECTIVOS.LOCAL>
2019-09-05 10:54:38 -0500

    varios

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=bdproyectmetis',
+    'dsn' => 'mysql:host=localhost;dbname=bdprojectderco',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
